d1b8a3e07f6ce40f48d12393ec7f43fe70e74e5e
Manoel-iranilson <manoelvieirafilho0@gmail.com>
2024-11-07 17:51:44 -0300

    fix: create user

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,13 +1,13 @@
 import { Controller, Get, Post, Body } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
-// import { IsPublic } from 'src/auth/decorators/is-public.decorator';
+import { IsPublic } from 'src/auth/decorators/is-public.decorator';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  // @IsPublic()
+  @IsPublic()
   @Post()
   create(@Body() createUserDto: CreateUserDto) {
     return this.userService.create(createUserDto);
